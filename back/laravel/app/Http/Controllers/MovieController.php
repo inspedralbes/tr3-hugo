20706a5f76dacc719998479de1bf9627e1edcf6c
a22hugtrigon <a22hugtrigon@inspedralbes.cat>
2024-03-11 11:36:40 +0100

    api_pelicules

---
+++ back/laravel/app/Http/Controllers/MovieController.php
@@ -0,0 +1,17 @@
+<?php
+
+// app/Http/Controllers/MovieController.php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
+class MovieController extends Controller
+{
+    public function index()
+    {
+        $movies = Movie::all();
+        return response()->json($movies);
+    }
+}
--- back/laravel/routes/api.php
+++ back/laravel/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+// routes/web.php o routes/api.php
+
+
+Route::get('/movies', [MovieController::class, 'index']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
